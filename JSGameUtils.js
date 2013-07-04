294f29b7641d68107558883e2fafcac9d5e0b1c4
corbbing <cgappdev@gmail.com>
2013-07-03 22:33:20 -0700

    Update JSGameUtils.js

--- JSGameUtils.js
+++ JSGameUtils.js
@@ -1,3 +1,9 @@
+//
+//The Hinge Object
+//
+//A simple hinge for animation and drawing;
+//
+
 function hinge(){
 
   this.x = 0;
@@ -13,3 +19,14 @@ hinge.prototype.getEnd = function(){
   this.endx = Math.sin(this.rot) * this.length + this.x;
   this.endy = Math.cos(this.rot) * this.length + this.y;
 }
+
+hinge.prototype.draw = function(){
+  
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(this.x, this.y);
+  ctx.lineTo(this.endx, this.endy);
+  ctx.restore();
+  
+}
+
